5f247f2719c7410e314f3615be2b685e5f4adb47
Anton Boldyrev <a.m.boldyrev@gmail.com>
2023-02-04 17:13:37 +0500

    feat: добавить слайдер для tracks
    на данный момент проблема с переходом с третьего слайда на первый

---
+++ scripts/slider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: "auto",
+    loopedSlides: 1,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.track__button--next',
+        prevEl: '.track__button--prev',
+    },
+});
+
